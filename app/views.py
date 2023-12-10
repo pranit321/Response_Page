d94ca077fa89720b8a4144ac8d2a2348fd299469
pranit <pranitsahu2000@gmail.com>
2023-12-10 17:49:18 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def pranit(request):
+    return render(request,'pranit.html')
+
+def subham(request):
+    return render(request,'subham.html')
+
+def swayam(request):
+    return render(request,'swayam.html')
